1fe4e11414e923f66dc2ba77060a7828ecbb4f2f
Serhii Yemets <Serhiiyemets2020@gmail.com>
2026-01-18 15:47:08 +0100

    app/notes/filte file commit

--- app/notes/filter/[...slug]/page.tsx
+++ app/notes/filter/[...slug]/page.tsx
@@ -1,14 +1,51 @@
 import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query';
 import { fetchNotes } from '@/lib/api';
 import NotesClient from './Notes.client';
+import type { Metadata } from "next";
 
 interface NotesPageProps {
-    params: Promise<{ slug: string[] }>;
+    params: { slug: string[] };
+}
+
+export async function generateMetadata(
+    { params }: NotesPageProps): Promise<Metadata> {
+    const { slug } = params;
+    const tag = slug?.[0] ?? 'all';
+
+    const title =
+        tag === 'all'
+            ? 'Note Hub — All Notes'
+            : `Note Hub — Notes: ${tag}`;
+
+    const description =
+        tag === 'all'
+            ? 'Browse all notes on Note Hub.'
+            : `Browse notes filtered by: ${tag}.`;
+
+    return {
+        title,
+        description,
+        openGraph: {
+            title,
+            description,
+            url: `https://notehub.com/notes/filter/${tag}`,
+            siteName: 'NoteHub',
+            images: [
+                {
+                    url: 'https://ac.goit.global/fullstack/react/notehub-og-meta.jpg',
+                    width: 1200,
+                    height: 630,
+                    alt: title,
+                },
+            ],
+            type: 'website',
+        },
+    };
 }
 
 export default async function NotesPage({ params }: NotesPageProps) {
-    const resolvedParams = await params;
-    const tag = resolvedParams.slug?.[0] ?? 'all';
+    const { slug } = params;
+    const tag = slug?.[0] ?? 'all';
 
     const queryClient = new QueryClient();
 
