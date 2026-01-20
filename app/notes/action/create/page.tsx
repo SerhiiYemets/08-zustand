836ae278207a38281b1d9bb41367cad5b5f9374b
Serhii Yemets <Serhiiyemets2020@gmail.com>
2026-01-20 02:39:08 +0100

    page file commit

--- app/notes/action/create/page.tsx
+++ app/notes/action/create/page.tsx
@@ -27,7 +27,7 @@ export default function CreateNotePage() {
         <main className={css.main}>
             <div className={css.container}>
                 <h1 className={css.title}>Create note</h1>
-                {/* <NoteForm /> */}
+                <NoteForm />
             </div>
         </main>
     );
